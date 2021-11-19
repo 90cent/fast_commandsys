059621ae91560ca721ecdc00863e86f69c3ec6ef
GEGAKE <psnreh0708@gmail.com>
2021-11-19 22:44:04 +0100

    fuck this shit

--- src/commandsystem.rs
+++ src/commandsystem.rs
@@ -1,6 +1,13 @@
+pub use loging::*;
+
+
 pub mod commandbase {
     use std::{fmt, io::Error, iter::FromFn, process::CommandArgs, thread};
     use tokio;
+    use lazy_static::*;
+
+    use crate::commandsystem::loging::*;
+
 
     pub struct Command {
         command_name: String,
@@ -9,7 +16,8 @@ pub mod commandbase {
     }
 
     pub trait CommandExecuter { 
-        fn execute(self);  // FUNC wird so irgendwie in der methode nochmal definiert. Rust halt 
+        fn execute(self);
+        fn execute_static(&'static self);
     }
 
     impl CommandExecuter for Command{
@@ -17,7 +25,22 @@ pub mod commandbase {
         {                         
             let action = self.command_action;                            
             let thread = thread::spawn(action);  
-            thread.join().unwrap();                                     
+
+            match thread.join() {   // Check if thread can be created
+                Ok(thread) => return thread,
+                Err(e) => {log(LogLevel::EXCEPTION, format!("Failed to execute Command. {:#?}",&e));},
+            };
+        }
+
+        fn execute_static(&'static self) // Neuen Thread mit der Funktion erstellen
+        {                         
+            let action = &self.command_action;                            
+            let thread = thread::spawn(action);  
+
+            match thread.join() {   // Check if thread can be created
+                Ok(thread) => return thread,
+                Err(e) => {log(LogLevel::EXCEPTION, format!("Failed to execute Command. {:#?}",&e));},
+            };
         }
     }
     
@@ -28,20 +51,54 @@ pub mod commandbase {
     }
 
     impl Command {
-        pub fn new(name: &'static str, desc: &'static str,action: impl Fn() -> () + Send + 'static + Sync) -> Self {
-            return  Command {
+        pub fn new(name: &'static str, desc: &'static str,action: impl Fn() -> () + Send + 'static + Sync) {
+            let cmd = Command {
                 command_name: name.into(),
                 command_description: desc.into(),
                 command_action: Box::new(action)
             };
+            manager::init(cmd)
+        }
+    }
+
+    mod manager {
+        use crate::commandsystem::commandbase::Command;
+        use std::{collections::{self, HashMap}, hash::Hash, io::read_to_string, sync::{Mutex, MutexGuard}};
+        use lazy_static::*;
+
+
+        lazy_static! {
+            pub static ref COMMANDS: Mutex<HashMap<String,Command>> = Mutex::new({
+                HashMap::new()
+            });
+        }
+        
+        pub fn init(cmd: Command)
+        {
+            lazy_static::initialize(&COMMANDS);
+            let name = cmd.command_name.clone();
+            let command = cmd;
+
+            &COMMANDS.lock().unwrap().insert(name, command);
+        }
+
+        pub fn get(name: String) -> Box<> {
+            lazy_static::initialize(&COMMANDS);
+
+            let c = match &COMMANDS.lock().unwrap().get(&name) {
+                Some(k) => k.to_owned(),
+                None => todo!(),
+            };
+
+            return c;
         }
     }
 }
 
-pub mod logging {
+pub mod loging {
     use termcolor::{self, Color};
     use std::io::{Write};
-
+    use chrono::{Datelike, Timelike, Utc};
 
     pub enum LogLevel {
         NORMAL,
@@ -50,28 +107,52 @@ pub mod logging {
         EXCEPTION
     }
 
-    struct Log {
+    pub struct Log {
         prefix: String,
         message: String,
         color: Color
     }
 
-    fn create_log_prefix() -> String {        // Macht ein prefix das so aussehen soll: [ZEIT UND DATUM - CommandName] 
+    fn create_log_prefix(label: &'static str) -> String {        // Macht ein prefix das so aussehen soll: [ZEIT UND DATUM - CommandName]
+        let utc = Utc::now();
+        let time = format!("{}:{}:{}",utc.hour(),utc.minute(),utc.second());
+        let date =format!("{}.{}.{}",utc.date(),utc.month(),utc.year());
 
+        return format!("[{} - {}] ({}):",time,date,label);
     }
 
-    pub fn log(level: LogLevel,message: String) {  // IMPORTANT: Include fmt::Display for Log to make it better and simpler
-        let color = match level {
+    pub fn log(level: LogLevel,message: String) -> Log {  // IMPORTANT: Include fmt::Display for Log to make it better and simpler
+        let logtolog = match level {
             LogLevel::NORMAL => {
                 Log {
-                    prefix: "[]".into(),
-                    message: "".into(),
+                    prefix: create_log_prefix(""),
+                    message: message,
                     color: termcolor::Color::White,
                 }
             },
-            LogLevel::INFORMATION => termcolor::Color::Green,
-            LogLevel::WARNING => termcolor::Color::Magenta,
-            LogLevel::EXCEPTION => todo!(),
+            LogLevel::INFORMATION => {
+                Log {
+                    prefix: create_log_prefix("INFORMATION"),
+                    message: message,
+                    color: termcolor::Color::Magenta,
+                }
+            },
+            LogLevel::WARNING => {
+                Log {
+                    prefix: create_log_prefix("WARN"),
+                    message: message,
+                    color: termcolor::Color::Yellow,
+                }
+            },
+            LogLevel::EXCEPTION => {
+                Log {
+                    prefix: create_log_prefix("EXCEPTION"),
+                    message: message,
+                    color: termcolor::Color::Red,
+                }
+            },
         };
+
+        return logtolog;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::{thread};
-
 mod commandsystem;
 use commandsystem::commandbase::*;
 
 
 fn main() {
     let mut cmd = Command::new("test", "test",testo);
-    println!("{}",&cmd);
-    cmd.execute();
+
+    
+    
 }
 
 fn testo() {
